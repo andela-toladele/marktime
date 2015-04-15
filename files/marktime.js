3954421b5627bb4e79fbc9212f2f305a896e805d
Ejiro <eawinifred@gmail.com>
2015-04-15 14:41:52 +0100

    User Time Logged

--- files/marktime.js
+++ files/marktime.js
@@ -1,3 +1,6 @@
+document.getElementById("companyname").innerHTML = "Andela"; //Set company name
+	now = new Date();
+
 document.getElementById('guest').onclick = function() {
 	document.getElementById('login').style.display = "none";
 	document.getElementById('guestlogin').style.display = "block";
@@ -43,26 +46,36 @@ document.getElementById('signin').onclick = function() {
 	var useremail = document.getElementById('useremail').value;
 	var password = document.getElementById('password').value;
 	var username;
-
+	var output;
+//Search for the correcr username and password
 	for (i=0;i<database.length;i++) {
 		if (useremail == database[i].email && password == database[i].password) {
-			username = database[i].name;
+			currentuser = database[i];
+			username = currentuser.name;
+			currentuser.latestStateChange = now.getHours() + ":" + now.getMinutes();
+			currentuser.status = "Present";
+
 		}
-	};
-
-	$('#myModal').modal('hide');
-	$('#myModal').on('hidden', function () {
-	  // Load up a new modal...
-	  $('#myModalNew').modal('show')
-	})
-
+	}
 
 	if (username == null) {
-		console.log("Sorry, user not found. Please try again or sign in as a guest");
+		document.getElementById("output").innerHTML = "<h2>Sorry, user not found. Please try again or sign in as a guest</h2>";
 	}
-	else console.log("Welcome " + username);
 
+	else {
+		var yourtime = currentuser.latestStateChange;
+		document.getElementById("output").innerHTML = "<h1> Welcome "+ username +"</h1><h2> You signed in at " + yourtime+ "</h2>";
+	}
 
+//Show confirmation modal
+	$('#signInModal').modal('hide');
+	$('#confirmModal').modal('show');
+
+//Go back to signin on error
+	document.getElementById('errorback').onclick = function() {
+		$('#confirmModal').modal('hide');
+		$('#signInModal').modal('show');
+	};
 
 
 };
